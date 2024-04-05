7cbb5e43aad749440391899a9376a047ede0bfcf
Lucas L. Lima <lucaslopes.developer@gmail.com>
2024-04-04 22:16:10 -0300

    working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 const words = ['Palavra 1', 'Palavra 2', 'Palavra 3', 'Palavra 4', 'Palavra 5', 'Palavra 6', 'Palavra 7', 'Palavra 8', 'Palavra 9', 'Palavra 10', 'Palavra 11', 'Palavra 12','Palavra 13', 'Palavra 14', 'Palavra 15', 'Palavra 16', 'Palavra 17', 'Palavra 18'];
+
 const carousel = document.querySelector('.carousel');
 let itemCounter = 0
+
 words.forEach(word => {
     const span = document.createElement('span');
     span.className = 'carousel-item';
@@ -11,19 +13,26 @@ words.forEach(word => {
 
 });
 
-// Código para o carrossel infinito com sistema de fila
-setInterval(() => {
-    const firstItem = carousel.firstElementChild;
-    firstItem.style.transition = "none";
-    firstItem.style.transform = "translateX(-100%)";
+$(document).ready(function(){
+    $('.carousel-slick').slick({
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 0,
+      speed: 8000,
+      //waitForAnimate: false,
+      //accessibility: false,
+      arrows: false,
+      cssEase: 'linear',
+      //easing: 'linear',
+      //draggable: true,
+      //pauseOnHover: true,
 
-    setTimeout(() => {
-        firstItem.style.transition = "transform 0.2s ease";
-        firstItem.style.transform = "translateX(10px)";
-        carousel.appendChild(firstItem);
-    }, 1);
-}, 1000); // Tempo em milissegundos
-
+      
+    });
+  });
+  
 
 
 
